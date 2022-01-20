21f995341d97b26b4ea17676a769936d5e481929
Jason Ho <jason.ho@obf.ateneo.edu>
2022-01-20 20:58:52 +0800

    Restrict address to read only

--- L2_getter_setter/getter_setter.rb
+++ L2_getter_setter/getter_setter.rb
@@ -1,5 +1,6 @@
 class Profile
-  attr_accessor :full_name, :age, :address, :work
+  attr_accessor :full_name, :age, :work
+  attr_reader :address
 
   def initialize(full_name, age, address, work)
     @full_name = full_name
