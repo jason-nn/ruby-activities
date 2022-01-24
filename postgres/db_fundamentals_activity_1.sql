17f512f27559ba261f087610bc8f6abb9f7f4499
Jason Ho <jason.ho@obf.ateneo.edu>
2022-01-24 21:15:19 +0800

    Add database fundamentals activity 1

---
+++ postgres/db_fundamentals_activity_1.sql
@@ -0,0 +1,91 @@
+create table students (
+  id integer PRIMARY KEY,
+  first_name character varying(255) NOT NULL,
+  middle_name character varying(255) NOT NULL,
+  last_name character varying(255) NOT NULL,
+  age integer NOT NULL,
+  location character varying(255) NOT NULL
+);
+
+insert into
+  students (
+    id,
+    first_name,
+    middle_name,
+    last_name,
+    age,
+    location
+  )
+values
+  (1, 'Juan', 'Blank', 'Cruz', 18, 'Manila');
+
+insert into
+  students (
+    id,
+    first_name,
+    middle_name,
+    last_name,
+    age,
+    location
+  )
+values
+  (2, 'Anne', 'Blank', 'Wall', 20, 'Manila');
+
+insert into
+  students (
+    id,
+    first_name,
+    middle_name,
+    last_name,
+    age,
+    location
+  )
+values
+  (3, 'Theresa', 'Blank', 'Joseph', 21, 'Manila');
+
+insert into
+  students (
+    id,
+    first_name,
+    middle_name,
+    last_name,
+    age,
+    location
+  )
+values
+  (4, 'Isaac', 'Blank', 'Gray', 19, 'Laguna');
+
+insert into
+  students (
+    id,
+    first_name,
+    middle_name,
+    last_name,
+    age,
+    location
+  )
+values
+  (5, 'Zack', 'Blank', 'Matthews', 22, 'Marikina');
+
+insert into
+  students (
+    id,
+    first_name,
+    middle_name,
+    last_name,
+    age,
+    location
+  )
+values
+  (6, 'Finn', 'Blank', 'Lam', 25, 'Manila');
+
+update
+  students
+set
+  first_name = 'Ivan',
+  middle_name = 'Ingram',
+  last_name = 'Howard',
+  age = 25,
+  location = 'Bulacan'
+where
+  id = 1;
