7d3360dee790443f3089ef54acf96ddb9bab8a4b
Jason Ho <jason.ho@obf.ateneo.edu>
2022-02-24 20:00:40 +0800

    Add test

--- recursive_multiplication.rb
+++ recursive_multiplication.rb
@@ -3,6 +3,6 @@ def multiply(a, b)
   return a + multiply(a, b - 1)
 end
 
-puts multiply(5, 10)
-puts multiply(2, 3)
-puts multiply(7, 19)
+puts multiply(5, 10) === 5 * 10
+puts multiply(2, 3) === 2 * 3
+puts multiply(7, 19) === 7 * 19
